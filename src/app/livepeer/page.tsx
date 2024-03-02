210cf8f5f9dfc26cedb65dcfb7f604b57ebcb050
financegeek-org <nlai@rediffmail.com>
2024-03-02 00:10:39 -0800

    collaborative filtering

--- src/app/livepeer/page.tsx
+++ src/app/livepeer/page.tsx
@@ -1,7 +1,22 @@
 import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
+import {cFilter} from "../../lib/cf.js";
 
 export default async function Home() {
+  const ratings = [
+    [1, 1, 0, 0, 1],
+    [1, 1, 0, 0, 0],
+    [1, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1],
+    [0, 0, 1, 1, 0],
+  ];
+
+  const user1=cFilter(ratings,1);
+  const user2=cFilter(ratings,2);
+  const user3=cFilter(ratings,3);
+  const user4=cFilter(ratings,4);
+
+
   return (
     <main className="flex flex-col md:flex-row min-h-screen justify-center items-center bg-black gap-12 p-10">
       <div className="-translate-y-10 flex flex-col flex-1 justify-end md:items-end gap-3">
@@ -17,6 +32,18 @@ export default async function Home() {
           </Link>
           <Link
             className="items-center justify-center md:justify-end gap-2 flex-1 flex text-lg hover:text-white/80 text-white/90"
+            href="/livepeer/player/asset-short"
+          >
+            <span>Play a wholesome video</span> <ArrowUpRight className="w-5 h-5" />
+          </Link>
+          <Link
+            className="items-center justify-center md:justify-end gap-2 flex-1 flex text-lg hover:text-white/80 text-white/90"
+            href="/livepeer/player/asset-long"
+          >
+            <span>Play a degen video</span> <ArrowUpRight className="w-5 h-5" />
+          </Link>
+          <Link
+            className="items-center justify-center md:justify-end gap-2 flex-1 flex text-lg hover:text-white/80 text-white/90"
             href="/livepeer/player/bad-account"
           >
             <span>Play a scam video</span>
@@ -26,20 +53,26 @@ export default async function Home() {
             className="items-center justify-center md:justify-end gap-2 flex-1 flex text-lg hover:text-white/80 text-white/90"
             href="/livepeer/player/jwt"
           >
-            <span>Play a JWT-protected video</span>
+            <span>Play a JWT-protected Disney video</span>
             <ArrowUpRight className="w-5 h-5" />
           </Link>
         </div>
       </div>
       <span className="w-full h-px md:h-[600px] md:w-px -rotate-[30deg] md:rotate-[35deg] bg-gradient-to-r md:bg-gradient-to-b from-transparent via-white/80 to-transparent" />
       <div className="translate-y-10 flex flex-col flex-1 items-start gap-3">
-        <h1 className="md:flex-1 flex text-3xl font-medium">Broadcast</h1>
-        <Link
-          className="items-center gap-2 md:flex-1 flex text-lg hover:text-white/80 text-white/90"
-          href="/livepeer/broadcast"
-        >
-          <span>Broadcast now</span> <ArrowUpRight className="w-5 h-5" />
-        </Link>
+        <h1 className="md:flex-1 flex text-3xl font-medium">Collaborative Filtering Recommendations Engine</h1>
+Given ratings = [
+  [1, 1, 0, 0, 1],
+  [1, 1, 0, 0, 1],
+  [1, 0, 0, 0, 1],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 0],
+  [0, 0, 1, 1, 0],
+];
+          <span>Recommended Video for user 1 is {user1[0]+1}</span>
+          <span>Recommended Video for user 2 is {user2[0]+1}</span>
+          <span>Recommended Video for user 3 is {user3[0]+1}</span>
+          <span>Recommended Video for user 4 is {user4[0]+1}</span>
       </div>
     </main>
   );
